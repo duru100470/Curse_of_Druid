7f47832bd2aeff23d7d29f4313b12125fd62487a
duru100470 <zenpia@naver.com>
2023-01-06 00:15:02 +0900

    Structure 구현

---
+++ Curse_of_Druid/Assets/Scripts/Structure/DebugStructure.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugStructure : Structure, IFlammable
+{
+    public void Burn(int burnTime)
+    {
+    }
+
+    public void SpreadFlame()
+    {
+    }
+}
