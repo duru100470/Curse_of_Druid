fa117245ce4ff468aa4c4f247cc07d06ccdc9d14
Hena <hena0914@korea.ac.kr>
2023-02-23 01:38:58 +0900

    passing platform 버그 수정

--- Curse_of_Druid/Assets/Scripts/Tile/PassingPlatform.cs
+++ Curse_of_Druid/Assets/Scripts/Tile/PassingPlatform.cs
@@ -16,11 +16,17 @@ public class PassingPlatform : RuleTile
         if (Input.GetKey(KeyCode.Space) && Input.GetButton("Vertical"))
         {
             platformObject.rotationalOffset = 180f;
+            Invoke("Restore", 0.3f);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             platformObject.rotationalOffset = 0f;
         }
+    }
+
+    void Restore()
+    {
+        platformObject.rotationalOffset = 0f;
     }
 }
