1728bb7c3d9b21edfa43df4fdde50d646326eb7f
duru100470 <zenpia@naver.com>
2023-02-22 18:22:56 +0900

    DestroyCallback + Vine 버그 픽스

--- Curse_of_Druid/Assets/Scripts/Obstacle/Vine.cs
+++ Curse_of_Druid/Assets/Scripts/Obstacle/Vine.cs
@@ -36,6 +36,8 @@ public class Vine : Obstacle
                 vineTile.Obstacle = this;
                 vineTile.DestroyCallback = DestroyTile;
 
+                childrenTileList.Add(vineTile);
+
                 // fixme
                 // TileManager에 타일 생성 알려야함
                 TileManager.Inst.AddTile(growingPoint, vineTile);
@@ -49,17 +51,18 @@ public class Vine : Obstacle
     public override void DestroyTile(Coordinate coor)
     {
         // 부서진 풀 타일보다 위에 있는 타일들 찾기
-        var aboveGrasses = childrenTileList.Where(e => e.Pos.Y > coor.Y).ToList();
+        var belowGrasses = childrenTileList.Where(e => e.Pos.Y < coor.Y).ToList();
 
         // 생장점 다시 내리기
         growingPoint = coor;
 
         // 위 타일 제거
-        for (int i = aboveGrasses.Count - 1; i >= 0; i--)
+        for (int i = belowGrasses.Count - 1; i >= 0; i--)
         {
             // Callback 제거해서 이 함수가 불리지 않도록
-            aboveGrasses[i].DestroyCallback = null;
-            aboveGrasses[i].Destroy();
+            belowGrasses[i].DestroyCallback = null;
+            childrenTileList.Remove(belowGrasses[i]);
+            belowGrasses[i].Destroy();
         }
 
         base.DestroyTile(coor);
